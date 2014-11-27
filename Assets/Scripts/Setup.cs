e1533e7bc1af3b4720cded4aaee8f8d0a1e422ea
Joseph Page <josefpaij@josef-mbp-4.local>
2014-11-26 23:09:26 -0500

    git game setup

---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Setup : MonoBehaviour {
+
+    public GameObject _cellPrefab;
+    public GameObject[,] _cells;
+    public int _cellDistance;
+    private int tempCellDistance;
+
+    const int boardSize = 8;
+
+	// Use this for initialization
+	void Start () {
+        _cells = new GameObject[8,8];
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                _cells[x,y] = (GameObject) Instantiate (
+                        _cellPrefab, 
+                        new Vector3((4-x)*_cellDistance, 0, (y-4)*_cellDistance), 
+                        Quaternion.identity); 
+                Debug.Log("Cell created");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (_cellDistance != tempCellDistance) {
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    _cells[x,y].transform.position = new Vector3((4-x)*_cellDistance, 0, (y-4)*_cellDistance);
+                }
+            }
+            tempCellDistance = _cellDistance;
+            Debug.Log("Cell created");
+        }
+    }
+}
